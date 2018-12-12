7a1e41a6db3b0a9507c7b4649bb83511fc9826d1
Oleg Ivanov <ivanov@megaputer.ru>
2018-12-12 18:09:32 +0300

    Typo fixed

--- responses/ProjectExecutionStatistics.go
+++ responses/ProjectExecutionStatistics.go
@@ -2,7 +2,7 @@ package responses
 
 import "github.com/gluk-skywalker/polyanalyst6api-go/objects"
 
-// ProjectExecutionStatistics is the struct for `/project/execution-statistics` request resoibse
+// ProjectExecutionStatistics is the struct for `/project/execution-statistics` request response
 type ProjectExecutionStatistics struct {
 	Nodes           []objects.NodeStatistics `json:"nodes"`
 	NodesStatistics objects.NodesStatistics  `json:"nodesStatistics"`
