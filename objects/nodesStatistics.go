322a676ab5a9d34480260cf623588dfbc19cba2c
Oleg Ivanov <ivanov@megaputer.ru>
2019-04-22 12:47:42 +0300

    proper comment

--- objects/nodesStatistics.go
+++ objects/nodesStatistics.go
@@ -1,6 +1,6 @@
 package objects
 
-// NodesStats is the structure for overall nodes statistics
+// NodesStatistics is the structure for overall nodes statistics
 type NodesStatistics struct {
 	EmtpyNodesCount          uint `json:"emtpyNodesCount"`
 	SynchronizedNodesCount   uint `json:"synchronizedNodesCount"`
