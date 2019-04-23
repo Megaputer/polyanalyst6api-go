1bd3ede0282d6ec2a32e0d9e36703ddb61a3980f
Oleg Ivanov <ivanov@megaputer.ru>
2019-04-23 12:53:59 +0300

    NodeStatistics: DatasetRows & DatasetCols fields

--- objects/nodeStatstics.go
+++ objects/nodeStatstics.go
@@ -2,7 +2,7 @@ package objects
 
 // NodeStatistics is the structure for node statistics
 type NodeStatistics struct {
-	ID                int64    `json:"id"`
+	ID                int64   `json:"id"`
 	Type              string  `json:"type"`
 	Name              string  `json:"name"`
 	Status            string  `json:"status"`
@@ -10,6 +10,8 @@ type NodeStatistics struct {
 	StartTime         float64 `json:"startTime"`
 	EndTime           float64 `json:"endTime"`
 	Duration          float64 `json:"duration"`
+	DatasetRows       int64   `json:"datasetRows"`
+	DatasetCols       int64   `json:"datasetCols"`
 	FreeMemoryInitial float64 `json:"freeMemoryInitial"`
 	FreeMemoryFinal   float64 `json:"freeMemoryFinal"`
 	FreeDiscInitial   float64 `json:"freeDiscInitial"`
