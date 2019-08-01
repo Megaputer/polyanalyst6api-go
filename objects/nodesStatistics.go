700882f4fb5816c6d38b8a597375db095de94c82
Oleg Ivanov <ivanov@megaputer.ru>
2019-08-01 14:56:40 +0300

    FIX: typo in NodesStatistics struct corrected

--- objects/nodesStatistics.go
+++ objects/nodesStatistics.go
@@ -2,7 +2,7 @@ package objects
 
 // NodesStatistics is the structure for overall nodes statistics
 type NodesStatistics struct {
-	EmtpyNodesCount          int `json:"emtpyNodesCount"`
+	EmtpyNodesCount          int `json:"emptyNodesCount"`
 	SynchronizedNodesCount   int `json:"synchronizedNodesCount"`
 	UnsynchronizedNodesCount int `json:"unsynchronizedNodesCount"`
 }
