e491a6b2b003e125973535464aef4be42ed8808d
Oleg Ivanov <ivanov@megaputer.ru>
2019-11-08 14:03:26 +0300

    IsDBRecoveryError func + simiral function rename

--- func.go
+++ func.go
@@ -10,11 +10,21 @@ func SetMainErrorProcessing() {
 	branch = "main"
 }
 
-// IsPABUSY determines if this is a PABUSY error
-func IsPABUSY(err error) bool {
+// IsPABUSYError determines if this is a PABUSY error
+func IsPABUSYError(err error) bool {
 	serverError, ok := err.(ServerError)
 	if !ok {
 		return false
 	}
 	return serverError.Code == 503
+}
+
+// IsDBRecoveryError determines if this is a DBRecovery error
+func IsDBRecoveryError(err error) bool {
+	serverError, ok := err.(ServerError)
+	if !ok {
+		return false
+	}
+	msg := "PolyAnalyst Server performs database recovery and cannot establish connection right now. Please wait a minute and retry operation"
+	return serverError.Message == msg
 }
