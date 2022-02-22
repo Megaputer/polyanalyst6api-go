34a37eff6479a1a2ecdebbf75c0f5f7c6dfe591b
Oleg Ivanov <ivanov@megaputer.ru>
2022-02-22 13:49:00 +0300

    node and nodeStatistics are extended with the Subtype attribute

--- objects/node.go
+++ objects/node.go
@@ -2,9 +2,10 @@ package objects
 
 // Node is for node data
 type Node struct {
-	ID     int    `json:"id"`
-	Type   string `json:"type"`
-	Name   string `json:"name"`
-	Status string `json:"status"`
-	ErrMsg string `json:"errMsg"`
+	ID      int    `json:"id"`
+	Type    string `json:"type"`
+	Subtype string `json:"subtype"`
+	Name    string `json:"name"`
+	Status  string `json:"status"`
+	ErrMsg  string `json:"errMsg"`
 }
--- objects/nodeStatstics.go
+++ objects/nodeStatstics.go
@@ -2,16 +2,17 @@ package objects
 
 // NodeStatistics is the structure for node statistics
 type NodeStatistics struct {
-	ID                int   `json:"id"`
+	ID                int     `json:"id"`
 	Type              string  `json:"type"`
+	Subtype           string  `json:"subtype"`
 	Name              string  `json:"name"`
 	Status            string  `json:"status"`
 	ErrMsg            string  `json:"errMsg"`
 	StartTime         float64 `json:"startTime"`
 	EndTime           float64 `json:"endTime"`
 	Duration          float64 `json:"duration"`
-	DatasetRows       int   `json:"datasetRows"`
-	DatasetCols       int   `json:"datasetCols"`
+	DatasetRows       int     `json:"datasetRows"`
+	DatasetCols       int     `json:"datasetCols"`
 	FreeMemoryInitial float64 `json:"freeMemoryInitial"`
 	FreeMemoryFinal   float64 `json:"freeMemoryFinal"`
 	FreeDiscInitial   float64 `json:"freeDiscInitial"`
